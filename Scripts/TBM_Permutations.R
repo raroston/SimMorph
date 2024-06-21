d69578cc8a5f12aecf4b4613a17126713f9b09b8
raroston <122128818+raroston@users.noreply.github.com>
2024-06-20 19:51:03 -0700

    Update TBM_Permutations.R

--- Scripts/TBM_Permutations.R
+++ Scripts/TBM_Permutations.R
@@ -1,3 +1,7 @@
+# TBM minimum sample size and permutations
+# Author: Rachel A. Roston
+# Date: 2024-06-20
+
 remove(list = ls())
 gc()
 
@@ -10,9 +14,19 @@ setwd(wd)
 source("Scripts/Repository/jacobianStats.R")
 source("Scripts/Repository/makePermutationSets.R")
 
-eN = 21
-maxOverlap = 15
+eN = 21 #for sample size n=21
 
+# Make permutations and save as a .csv
+  # maxOverlap = 15 #maximum overlapping subjects between sets
+  # sampleorder = makeSets(1:30,
+  #                       nsamples = eN,
+  #                       nsets = 5,
+  #                       overlap = maxOverlap)
+  # sampleorder = as.data.frame(sampleorder)
+  # colnames(sampleorder) <- c(paste0("Trial0", 1:5))
+  # write.csv(sampleorder, paste0(dir.out, "/N", eN, "_sampleorder.csv"))
+
+# Run TBM analysis with multiple sets of subjects, 
 ctrl = "baseline"
 experiment = c("Heart173",
                "Heart133",
@@ -25,25 +39,17 @@ experiment = c("Heart173",
 region = c("wholebody", "thorax")
 maskfile = paste0("Masks/Embryo_Atlas_mask_", region, ".nrrd")
 dir.data = "/SimMorph/Data/"
-dir.out = paste0("Results/JacobianAnalysis/Permutation_N",eN, "/")
+dir.out = paste0("Results/TBM/Permutation_N",eN, "/")
 subjects = read.csv("/SimMorph/Project_Design/subjects.csv")[,2]
 
 if(!dir.exists(dir.out)) dir.create(dir.out)
 
-sampleorder = makeSets(1:30,
-                       nsamples = eN,
-                       nsets = 5,
-                       overlap = maxOverlap)
-
-sampleorder = as.data.frame(sampleorder)
-colnames(sampleorder) <- c(paste0("Trial0", 1:5))
-write.csv(sampleorder, paste0(dir.out, "/N", eN, "_sampleorder.csv"))
-
 # Read jacs
+sampleorder = paste0(dir.out, "/N", eN, "_sampleorder.csv")
 ctrls = vector()
 jacs.ctrls = list()
 
-ctrls = paste0(dir.data, "/", ctrl, "/JacobianDeterminants/", subjects, "_", ctrl, "_Jacobian.nrrd")
+ctrls = paste0(dir.data, "/", ctrl, "/TBM/", subjects, "_", ctrl, "_Jacobian.nrrd")
 file.exists(ctrls)
 for(i in 1:length(ctrls)) jacs.ctrls[[i]] = antsImageRead(ctrls[i])
 
