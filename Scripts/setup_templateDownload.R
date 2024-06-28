ed60336e04a0f3b950183032bcdaba79690673f1
raroston <raroston@gmail.com>
2024-06-27 21:15:45 -0700

    Update ProjectDesign and scripts

--- Scripts/setup_templateDownload.R
+++ Scripts/setup_templateDownload.R
@@ -1,19 +1,16 @@
 # Set up SimMorph
 # Author: Rachel A. Roston, Ph.D.
 
-wd = "" #Working directory of repository clone
-setwd(wd)
-
 # Create directory structure
-directories = c("SimMorph/Data/baseline/CT",
-                "SimMorph/Data/Atlas")
+directories = c("./Data/baseline/CT",
+                "./Data/Atlas")
 lapply(X = directories, FUN = dir.create)
 
 #Download reference image
 
 download.file(url = "http://repo.mouseimaging.ca/repo/Embryo_Atlas_CT_E15.5_nifti/final.model.nii", 
-              destfile = "SimMorph/Data/Template/E15_template.nii", 
+              destfile = "./Data/Reference/Embryo_Atlas.nii", 
               method = "curl")
 download.file(url = "http://repo.mouseimaging.ca/repo/Embryo_Atlas_CT_E15.5_nifti/Embryo_Atlas_labels.nii", 
-              destfile = "SimMorph/Data/Template/E15_template_labels.nii", 
+              destfile = "SimMorph/Data/Reference/Embryo_Atlas_labels.nii", 
               method = "curl")
