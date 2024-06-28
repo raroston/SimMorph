b89e3db4ac3faabdd1f5c56b41180948a4f45679
raroston <raroston@gmail.com>
2024-06-28 11:20:41 -0700

    Updated scripts

--- Scripts/SimMorph_setup_templateDownload.R
+++ Scripts/SimMorph_setup_templateDownload.R
@@ -1,13 +1,11 @@
-# Set up SimMorph
+# Initial setup of SimMorph
 # Author: Rachel A. Roston, Ph.D.
 
-# Create directory structure
-directories = c("./Data/baseline/CT",
-                "./Data/Atlas")
+# Create directory for baseline images. Download baseline images from www.mousephenotype.org
+directories = c("./Data/baseline/CT")
 lapply(X = directories, FUN = dir.create)
 
-#Download reference image
-
+#Download reference image files
 download.file(url = "http://repo.mouseimaging.ca/repo/Embryo_Atlas_CT_E15.5_nifti/final.model.nii", 
               destfile = "./Data/Reference/Embryo_Atlas.nii", 
               method = "curl")
